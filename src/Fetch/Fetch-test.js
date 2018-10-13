ff669a52066e3c82d9d9e2f95cfd993915b62dc8
Laura Shamus <lkshamus@gmail.com>
2018-10-13 14:24:51 -0600

    Update App test with new state from fetch call to pass

---
+++ src/Fetch/Fetch-test.js
@@ -0,0 +1,48 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import { shallow } from 'enzyme'
+
+// describe('FetchCall', () => {
+
+//  it('should call fetch with the correct parameters', async () => {
+//   let mockPeople = [{ name: 'Luke Skywalker', homeworld: Tatooine }]
+
+//   const expected = ['https://swapi.co/api/people']
+
+//   window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+//       ok: true, json: () => Promise.resolve({
+//         mockPeople
+
+//   expect(window.fetch).toHaveBeenCalledWith(...expected)
+//       })
+//     }))
+// })
+ 
+// })
+
+
+
+ // mockEvent = { preventDefault: jest.fn() }
+    // mockUpdateGroceryList = jest.fn()
+    // let mockPeople = [{ name: 'Luke Skywalker', homeworld: Tatooine }]
+    // let mockPlanets = [{name: 'Tatoonie', name: 'Naboo'}]
+    // let mockVehicles = [{name: 'Sand Crawler', name: 'T-16'}]
+    // let mockSpecies = [{name: 'human', name: 'driod'}]
+   
+    // window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+    //   ok: true, json: () => Promise.resolve({
+    //     mockPeople
+    //   })
+    // }))
+    // const renderedComponent = shallow(<App
+    //                                   updateGroceryList={mockUpdateGroceryList}
+    //                                 />)
+  
+
+
+
+
+
+ //  })
+   
--- src/PeopleCard/PeopleCard.js
+++ src/PeopleCard/PeopleCard.js
@@ -3,16 +3,12 @@ import React, { Component } from "react";
 const PeopleCard = ({people, planets, species}) => {
 let mapOver = people.results.map(person => {
 
-// let speciesOne = species.species1.name
-// let speciesTwo = species.species2.name
  let personName = person.name
-
  let personWorld = person.homeworld
  for(let planet in planets){
   let planetPage = planets[planet]
   for(let aPlanet in planetPage){
     let planetName = planetPage[aPlanet].name
-    // console.log(planetPage[aPlanet].population)
     if(personWorld === planetPage[aPlanet].url){
       for(let i in species){
         if(species[i].people.includes(person.url)){
