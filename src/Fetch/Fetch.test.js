9d6ce268274190354807815a29327a78b81ace82
Laura Shamus <lkshamus@gmail.com>
2018-10-13 14:59:41 -0600

    Add first fetch test

--- src/Fetch/Fetch.test.js
+++ src/Fetch/Fetch.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import fetchData from './Fetch'
 import { shallow } from 'enzyme'
 
 describe('FetchCall', () => {
 
  it('should call fetch with the correct parameters', async () => {
-  let mockPeople = {[{ name: 'Luke Skywalker', homeworld: Tatooine }]}
+  let mockPeople = [{ name: 'Luke Skywalker', homeworld: 'Tatooine' }]
 
   
   window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
