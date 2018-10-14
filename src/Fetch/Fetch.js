a0d2d48d020776d1db0a816bcdb6ffd2599d578e
Nick Dambrosio <ndambro303@gmail.com>
2018-10-14 13:11:00 -0600

    Add planet.js boilerplate

--- src/Fetch/Fetch.js
+++ src/Fetch/Fetch.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 
-const fetchData = async (url) => {
+const fetchData = async url => {
   const data = await fetch(url);
   const response = await data.json();
   return response;
-} 
+};
 
 export default fetchData;
