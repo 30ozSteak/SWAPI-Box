244b6481e3fff3345ac7373d3e276aacc0f51b31
Nick Dambrosio <ndambro303@gmail.com>
2018-10-13 18:26:54 -0600

    clean up People.js, add Loading Component

--- src/People/People.js
+++ src/People/People.js
@@ -8,13 +8,12 @@ const People = ({ people, planets, species }) => {
     const characters = people.results.map(person => (
       <PeopleCard people={people} species={species} planets={planets} />
     ));
-    return <div className="people-container">{characters}</div>;
+    let charactersToRender = characters.slice(0, 1);
+    return <div className="people-container">{charactersToRender}</div>;
   } catch {
     return (
       <div>
-        <h1>
-          <Loading />
-        </h1>
+        <Loading />
       </div>
     );
   }
