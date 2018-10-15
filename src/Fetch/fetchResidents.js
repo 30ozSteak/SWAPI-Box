c2c25dde6d83862208e37edb1fea56378870bd84
Laura Shamus <lkshamus@gmail.com>
2018-10-14 18:06:46 -0600

    Begin the fetch call for residents

---
+++ src/Fetch/fetchResidents.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import fetchData from "./Fetch";
+
+let pageNumbers = ['5/', '68/', '81/', '26/', '30/', '3/', '21/', '36/', '31/', '38/', '39/', '42/', '60/', '61/', '66/', '35/', '34/', '55/', '74/', '22/', '72/', '73/', '63/']
+let dataResults = []
+let url = 'https://swapi.co/api/people/'
+
+const getResidentData = async () => {
+  for(let page in pageNumbers){
+    let value = await fetchData(url + pageNumbers[page])
+    dataResults.push(value)
+  }
+}
+
+export default getResidentData
