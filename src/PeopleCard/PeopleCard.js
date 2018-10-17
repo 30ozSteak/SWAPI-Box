2efd6f8a474a4a31df7148bbd1dbdb8a0876b2cd
Laura Shamus <lkshamus@gmail.com>
2018-10-17 17:01:33 -0600

    Refactor peoplecard function

--- src/PeopleCard/PeopleCard.js
+++ src/PeopleCard/PeopleCard.js
@@ -3,33 +3,31 @@ import "./PeopleCard.css";
 
 const PeopleCard = ({ people, planets, species }) => {
   let characterArray = [];
-
+  
   let answer = people.results.map(person => {
     let personName = person.name;
     let personWorld = person.homeworld;
-    for (let planet in planets) {
-      let planetPage = planets[planet];
-      planetPage.map(index => {
-        let indexNum = planetPage.indexOf(index);
-        let planetName = planetPage[indexNum].name;
-        if (personWorld === planetPage[indexNum].url) {
-          for (let i in species) {
+    planets.map(planet => {
+      planet.map(index => {
+        let indexPosition = planet.indexOf(index)
+        if(personWorld === planet[indexPosition].url){
+            for (let i in species) {
             if (species[i].people.includes(person.url)) {
               let peopleObject = {
                 name: personName,
-                location: planetName,
+                location: planet[indexPosition].name,
                 species: species[i].name,
-                population: planetPage[indexNum].population
+                population: planet[indexPosition].population
               };
               characterArray.push(peopleObject);
             }
           }
         }
-      });
-    }
-  });
+      })
+    })
+  })
 
-  const peopleStats = characterArray.map(data => {
+const peopleStats = characterArray.map(data => {
     return (
       <div className="card-literal">
         <h2>{data.name}</h2>
@@ -42,5 +40,6 @@ const PeopleCard = ({ people, planets, species }) => {
   });
 
   return <div>{peopleStats}</div>;
-};
+}
+
 export default PeopleCard;
