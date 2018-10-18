da78c8ac2a94b77dc46771e813b3cf2b9509604f
Nick Dambrosio <ndambro303@gmail.com>
2018-10-18 11:06:44 -0600

    change overflow and twinkle speed

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import MenuButtons from "../MenuButtons/MenuButtons";
 import "./Menu.css";
 import { Route, NavLink, Link } from "react-router-dom";
 
