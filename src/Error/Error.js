8a802d7f353360040852292ca19903d38822bcf9
Nick Dambrosio <ndambro303@gmail.com>
2018-10-20 14:19:56 -0600

    create error component

---
+++ src/Error/Error.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Error.css";
+
+const Error = ({ error }) => {
+  return (
+    <div className="error">
+      <h1>
+        4<div className="death" />4
+      </h1>
+    </div>
+  );
+};
